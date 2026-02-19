b8472a8e64ab690ff98577ae5c0cb73de3a7fab2
Полина Фёдорова <polina.com.r@yandex.ru>
2026-02-19 09:03:46 +0700

    Laba1

--- helper.py
+++ helper.py
@@ -7,9 +7,18 @@ def parse_line_shlex(line: str):
     resource = parts[0]
     date = datetime.strptime(parts[1], '%Y.%m.%d').date()
     value = float(parts[2])
-    return {
-        'resource': resource,
-        'date': date,
-        'value': value
-    }
+    return (
+        resource,
+        date,
+        value
+    )
+
+def newqwe(mass):
+    c=0
+    for i in mass:
+        if i%2==0:
+            c+=1
+    return c
+
+print(newqwe([1, 2, 3, 4]))
 
--- main.py
+++ main.py
@@ -7,4 +7,6 @@ with open('data.txt', 'r', encoding='utf-8') as f:
         if line:
             data.append(helper.parse_line_shlex(line))
 
-print(data)
+for i in data:
+    if i[2] > 100:
+        print(i)
